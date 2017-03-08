7dbce7d5095351ff30286c7d51e32edd7d37501e
ciec20151104688 <s20151104688@mac05.local>
2017-03-08 11:06:53 +0800

    Initial Commit

---
+++ 简易计算器/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  简易计算器
+//
+//  Created by liuyuxuan on 17/3/8.
+//  Copyright © 2017年 liuyuxuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
